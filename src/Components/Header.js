c31c8e65f56a2182098cc6db824844a0f2e71207
LF <adam-c1991@hotmail.com>
2023-01-19 17:42:36 +0000

    beginning of nav functionallity + hamburger CSS

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,7 @@
-import React from "react";
-import Nav from "./Nav";
-//import navJS from "../navJS";
+import react, { useState } from "react";
 
 const Header = () => {
-  //const navToggle = document.querySelector(".nav-toggle");
-
-  //   const navLinks = document.querySelectorAll(".nav__link");
-
-  //   navToggle.addEventListener("click", () => {
-  //     document.body.classList.toggle("nav-open");
-  //   });
-
-  //   navLinks.forEach((link) => {
-  //     link.addEventListener("click", () => {
-  //       document.body.classList.remove("nav-open");
-  //     });
-  //   });
+  const [navToggled, setNavToggled] = useState(false);
   <script src="navJS.js"></script>;
   return (
     <header>
@@ -25,11 +11,41 @@ const Header = () => {
       <button
         className="nav-toggle"
         aria-label="toggle navigation"
-        //onClick={this.navToggle}
+        onClick={() => {
+          setNavToggled(!navToggled);
+          console.log = "clicked";
+        }}
       >
-        <span className="hamburger"></span>
+        <span
+          className={
+            navToggled ? "hamburger hamburger::after" : "hamburger ::before"
+          }
+        ></span>
       </button>
-      <Nav />
+      <nav
+        className={
+          navToggled
+            ? "nav nav-open nav-toggle hamburger::after"
+            : " hamburger::before"
+        }
+      >
+        {
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#home" className="nav__link"></a>Home
+            </li>
+            <li className="nav__item">
+              <a href="#techStack" className="nav__link"></a>Tech Stack
+            </li>
+            <li className="nav__item">
+              <a href="#github" className="nav__link"></a>GitHub
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link"></a>Contact
+            </li>
+          </ul>
+        }
+      </nav>
     </header>
   );
 };
